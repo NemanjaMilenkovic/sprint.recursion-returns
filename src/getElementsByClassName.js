554b2798b48855833d06892f4f75e99bb03020b1
vianarafael <vianarafaelds@gmail.com>
2020-01-08 18:48:15 +0900

    finished getElementsByClassName()

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,5 +1,19 @@
-const getElementsByClassName = () => {
-  // YOUR CODE HERE
-};
+const getElementsByClassName = (targetName) => {
+  const result = [];
 
+  function findElements(element) {
+    if (element.classList && element.classList.contains(targetName)) {
+      result.push(element);
+    }
+
+    if (element.childNodes) {
+      for (const el of element.childNodes) {
+        findElements(el);
+      }
+    }
+  }
+
+  findElements(document.body);
+  return result;
+};
 module.exports = { getElementsByClassName };
